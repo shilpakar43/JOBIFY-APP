e8d85f9def408f87cf72aa87d6b614fac87cdb16
shilpakar43 <rikeshilpakar43@gmail.com>
2025-05-26 18:03:05 +0545

    signup form

--- src/component/index.js
+++ src/component/index.js
@@ -1,11 +1,11 @@
 import Signup from "./pages/Sign-up-page"
 
-import Login from "./pages/Login-page"
+// import Login from "./pages/Login-page"
 
 export {
   //pages
   Signup,
-  Login,
+  // Login,
 
 
 
